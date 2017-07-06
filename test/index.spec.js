02b15a246c473f924317f17e5997a337eda141a7
Daksh Miglani <hello@dak.sh>
2017-07-06 21:51:42 +0530

    fixed test case and version for travis

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,11 +1,11 @@
 const install = require('../index')
 const expect = require('chai').expect
 
-describe('Install Module', () => {
+describe('Install Module', function ()  {
     it('should export a function', () => {
         expect(install).to.be.a('function')
     })
-    it('should return a promise', () => {
+    it('should return a promise', function()  {
         expect(install()).to.be.a('promise')
     })
 })
